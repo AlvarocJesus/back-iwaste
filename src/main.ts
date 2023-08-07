4f90cabb639c168b46e7aea7d663280aaebf8b0e
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2023-08-07 18:37:56 -0300

    Adiciona configuracao para a documentacao do backend

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,27 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import 'reflect-metadata';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle('NestJS API')
+		.setDescription('The NestJS API description')
+		.setVersion('1.0')
+		.addTag('nestjs')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('docs', app, document);
+
 	app.useGlobalPipes(
 		new ValidationPipe({
 			whitelist: true,
 		}),
 	);
+
 	await app.listen(3000);
 }
 bootstrap();
